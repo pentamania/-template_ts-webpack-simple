4d19096fcfa0e43e480b3298f764d7855a2f321a
pentamania <tanigami7220@gmail.com>
2022-04-08 13:50:33 +0900

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ts-webpack-simple-template
+
+- Setup: `npm install`
+- Dev: `npm start`
+- Build: `npm run build`
+
+## ライセンス
+
+The Unlicense <http://unlicense.org/>
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, "src/index.html"),
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 9999,
+    host: "0.0.0.0",
+  },
+};
